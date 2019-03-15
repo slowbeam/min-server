59528a97c0d6547135944f5ae3cd51ab049390f5
slowbeam <sedwardscode@gmail.com>
2019-03-15 16:07:27 -0400

    set up basic express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+let express = require('express');
+
+let app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.info(`Server has started on ${PORT}`));
