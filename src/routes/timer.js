8daf5f57333c1a1c8365359ea48ab64ad54f225a
slowbeam <sedwardscode@gmail.com>
2019-03-15 16:36:06 -0400

    added queryString handler for Timer get requests

--- src/routes/timer.js
+++ src/routes/timer.js
@@ -1,13 +1,20 @@
 let express = require('express');
 let router = express.Router();
 
+// QueryString => query property on the request object
+// localhost:3000/timer?name=sandy&age=30
 router.get('/timer', (req, res) => {
-    res.send('You have requested a timer');
-})
-
+    if (req.query.name) {
+        res.send(`You have requested a timer ${req.query.name}`);
+    } else {
+        res.send('You have requested a timer');
+    }
+});
+// Params property on the request object
+// localhost:3000/timer/sandy
 router.get('/timer/:name', (req, res) => {
     res.send(`You have requested a timer ${req.params.name}`);
-})
+});
 
 
 
