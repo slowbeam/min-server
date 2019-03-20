6d196d2d819b25ddaaabbb559a901ce819a730f7
slowbeam <sedwardscode@gmail.com>
2019-03-20 12:19:19 -0400

    added update timer function to timers controller

--- src/controllers/timers.controller.js
+++ src/controllers/timers.controller.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Timer = require("../models/timer.model.js");
 
-// Public Route: 'api/v1/timers'
+// Public Route: GET 'api/v1/timers'
 exports.getTimers = (req, res) => {
     Timer.find()
     .then(timers => res.json(timers))
@@ -10,11 +10,40 @@ exports.getTimers = (req, res) => {
     );
 };
 
-// Public Route: 'api/v1/timers/:timer_id'
+// Public Route: GET 'api/v1/timers/:timer_id'
 exports.getTimer = (req, res) => {
     Timer.findById(req.params.timer_id)
     .then(timer => res.json(timer))
     .catch(err => 
         res.status(404).send({ notFound: "Did not find timer for this ID"})
     );
+};
+
+// Private Route: POST 'api/v1/timers'
+exports.createTimer = (req, res) => {
+    const {errors, isValid} = validateTimerInput(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    let timerFields = {};
+
+    timerFields.user = req.user.id;
+    timerFields.content = req.body.content;
+
+    new Timer(timerFields).save().then(timer => res.json(timer));
+};
+
+// Private Route: PUT 'api/v1/timers/:timer_id'
+exports.updateTimer = (req, res) => {
+    Timer.findById(req.params.timer_id).then(timer => {
+        let timerFields = {};
+
+        timerFields.content = req.body.content;
+
+        Timer.findOneAndUpdate(
+            { user: req.user.id},
+            { $set: timerFields },
+            { new: true }
+        ).then(timer => res.json(timer));
+    });
 };
