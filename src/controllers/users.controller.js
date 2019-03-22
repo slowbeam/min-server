d5cf69a2aef92a2b78d309720e4369c5344f0e1b
slowbeam <sedwardscode@gmail.com>
2019-03-22 16:09:21 -0400

    created user signUp route

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -6,4 +6,37 @@ const keys = require("../config/keys");
 const validateLoginInput = require("../validations/login.validation");
 const validateSignUpInput = require("../validations/signup.validation");
 
+// Public Route: POST 'api/users'
+exports.createUser = (req, res) => {
+    const { errors, isValid } = validateSignUpInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            errors.email = "Email already exists";
+            return res.status(400).json(errors);
+        } else {
+            let newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+};
+
+// Public Route 'api/users/login'
 
