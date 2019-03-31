595b3f9095adf2795ba0f22d130d75912af5b434
slowbeam <sedwardscode@gmail.com>
2019-03-31 16:28:08 -0400

    updated post route for timer to match adapted timer for pomodoro

--- src/controllers/timers.controller.js
+++ src/controllers/timers.controller.js
@@ -34,6 +34,16 @@ exports.createTimer = (req, res) => {
     timerFields.timerHours = req.body.timerHours;
     timerFields.timerMinutes = req.body.timerMinutes;
     timerFields.timerSeconds = req.body.timerSeconds;
+    timerFields.isPomodoro = req.body.isPomodoro;
+    timerFields.breakTime = req.body.breakTime || 0;
+    timerFields.breakLength = req.body.breakLength || 0;
+    timerFields.breakMinutes = req.body.breakMinutes || "00";
+    timerFields.longBreakTime = req.body.longBreakTime || 0;
+    timerFields.longBreakLength = req.body.longBreakLength || 0;
+    timerFields.longBreakMinutes = req.body.longBreakMinutes || "00";
+    timerFields.isBreak= req.body.isBreak || false;
+    timerFields.isLongBreak = req.body.isLongBreak || false;
+    timerFields.pomCount = req.body.pomCount || 0;
 
     new Timer(timerFields).save().then(timer => res.json(timer));
 };
@@ -42,8 +52,6 @@ exports.createTimer = (req, res) => {
 exports.updateTimer = (req, res) => {
     Timer.findById(req.params.timer_id).then(timer => {
         let timerFields = {};
-
-      
 
         if (req.body.user){
             timerFields.user = req.body.user;
