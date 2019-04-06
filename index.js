23397d57be81ab0f3e58ea461b12cbb7529201ee
Sandy <sedwardscode@gmail.com>
2019-04-06 09:31:56 -0400

    enabled cors preflight requests

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require('cors');
 const winston = require('winston');
 require('dotenv').config();
 
+// CORS middleware (enable all CORS requests)
+app.use(cors());
+app.options('*', cors());
+
 // Load startup modules
 require('./startup/logging')();
 require('./startup/routes')(app);
@@ -11,9 +15,6 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
-
-// CORS middleware (enable all CORS requests)
-app.use(cors());
 
 // Server
 const PORT = process.env.PORT || 3001;
